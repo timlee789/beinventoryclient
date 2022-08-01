ed05c1eaf13e4476fa9b07ebdbfab9ff289e2623
timlee789 <systeco@hotmail.com>
2022-08-01 15:21:45 -0400

    0801-2

--- components/binsearchresult.js
+++ components/binsearchresult.js
@@ -6,26 +6,11 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import cla from './form.module.css';
 
-function binSearchResult({searchResults}) {
+function BinSearchResult({searchResults}) {
   const { register, handleSubmit } = useForm();
   const [inputdata, setInputdata] = useState()
 
-  // async function onSubmit(data){ 
-  //   const res = await fetch('/api/posts',
-  //     {
-  //       method: 'PUT',
-  //       headers: {
-  //         'Content-type': 'application/json'
-  //       },
-  //       body: JSON.stringify({
-  //         id: data.id,
-  //         pcs_qty: searchResults.pcs_qty - data.pcs_qty,
-  //       })   
-  //     }
-  //   ) 
-  //   const data2 = await res.json()
-  //   setInputdata(data.pcs_qty)
-  // }
+
   async function deleteHanler(id) {
     const del = await fetch('/api/posts',
       {
@@ -60,21 +45,14 @@ function binSearchResult({searchResults}) {
         <Typography variant="h6" component="div">
         {data.product_name}
         </Typography>
-        {/* <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        UPC : {data.upc_code}
-        </Typography> */}
+       
         <Typography variant="body2">
         ITEM CODE: {data.item_code}
         </Typography>
         <Typography variant="h5">
        Qty: {data.pcs_qty}
         </Typography>
-        {/* <form onSubmit={handleSubmit(onSubmit)}>
-            <input type='number' {...register ('pcs_qty',{ valueAsNumber: true })} id='pcs_qty' className={cla.deductform}/>
-            <div className={cla.result}>{inputdata}</div>
-            <input type='hidden' {...register ('id')} defaultValue={data._id} ></input><br/>
-            <input type='submit'   className={cla.btn} ></input>
-        </form> */}
+      
         <form >
         <input type='hidden' defaultValue={data._id}></input>
         <button onClick={() => deleteHanler(data._id)}  className={cla.deletBtn}>DELETE</button>
@@ -93,4 +71,4 @@ function binSearchResult({searchResults}) {
   )
 }
 
-export default binSearchResult
+export default BinSearchResult
